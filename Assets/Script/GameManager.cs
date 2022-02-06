aee599c78455b2a8b21576ae3698c03b145bcdf8
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2022-02-06 12:13:10 -0800

    Created Manager scripts
    GameManager - sets up and manages game states
    
    MultiItemManager - stores multiple items of the same state and iterates through each one when new state matches the item state; used for cut scenes and mini games

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    MainMenu,
+    ControlScreen,
+    GamePlay,
+    CutScene,
+    MiniGame
+}
+
+/*
+ * Sets up and manages game state
+ */
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameState State;
+
+    public static event Action<GameState> OnGameStateChanged;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        //UpdateGameState(GameState.MainMenu);
+        UpdateGameState(GameState.CutScene);
+    }
+
+    public void UpdateGameState(GameState newState)
+    {
+        State = newState;
+        switch (newState)
+        {
+            case GameState.MainMenu:
+                break;
+            case GameState.ControlScreen:
+                break;
+            case GameState.GamePlay:
+                break;
+            case GameState.CutScene:
+                break;
+            case GameState.MiniGame:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+        OnGameStateChanged?.Invoke(newState);
+    }
+}
---
+++ Assets/Script/MultiItemManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiItemManager : MonoBehaviour
+{
+    [SerializeField] private GameState gameState;
+    [SerializeField] private GameObject[] items;
+    private int itemIndex;
+
+    void Awake()
+    {
+        itemIndex = 0;
+
+        //subscribe to event
+        GameManager.OnGameStateChanged += GameManagerOnGameStateChanged;
+    }
+
+    void OnDestroy()
+    {
+        GameManager.OnGameStateChanged -= GameManagerOnGameStateChanged;
+    }
+
+    private void GameManagerOnGameStateChanged(GameState state)
+    {
+        //deactivate previous game state item
+        if (itemIndex > 0)
+        {
+            items[itemIndex - 1].SetActive(false);
+        }
+
+        //set next item as current game state
+        if (itemIndex < items.Length)
+        {
+            items[itemIndex].SetActive(state == gameState);
+
+            if (state == gameState)
+            {
+                itemIndex++;
+            }
+        }
+    }
+}
