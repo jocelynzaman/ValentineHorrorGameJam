4f39c047860a13cc95ac9e21efad524671ee3762
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2022-02-13 22:09:22 -0800

    Created MenuManager script
    Manages state of main menu based on option that player chooses

---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameState gameState;
+    [SerializeField] private GameObject menu;
+
+    void Awake()
+    {
+        //subscribe to event
+        GameManager.OnGameStateChanged += GameManagerOnGameStateChanged;
+    }
+
+    void OnDestroy()
+    {
+        GameManager.OnGameStateChanged -= GameManagerOnGameStateChanged;
+    }
+
+    private void GameManagerOnGameStateChanged(GameState state)
+    {
+        menu.SetActive(state == gameState);        
+    }
+
+    public void PlayGame()
+    {
+        GameManager.Instance.UpdateGameState(GameState.CutScene);
+    }
+
+    public void ShowControls()
+    {
+        GameManager.Instance.UpdateGameState(GameState.ControlScreen);
+    }
+
+    public void ShowCredits()
+    {
+        GameManager.Instance.UpdateGameState(GameState.Credits);
+    }
+
+    //public void SetMenuOption(GameState state)
+    //{
+    //    GameManager.Instance.UpdateGameState(state);
+    //}
+
+    public void Back()
+    {
+        GameManager.Instance.UpdateGameState(GameState.MainMenu);
+    }
+
+    public void Exit()
+    {
+        print("quit game");
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
